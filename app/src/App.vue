97547e53fba8b998343c2288b236a67dc5c8706f
Kevin Gao <kevinyanggao@gmail.com>
2022-06-26 13:06:23 -0700

    add map, vue frontend

---
+++ app/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <nav>
+    <router-link to="/"></router-link>
+  </nav>
+  <router-view/>
+</template>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+      <!-- Search / Results -->
+      <div class="flex justify-center relative px-4 pt-0 pb-32 bg-hero-pattern bg-cover">
+          <div class="w-full max-w-screen-sm">
+              <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+              <div class="flex">
+                  <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+                         type="text"
+                         placeholder="Search for any IP address"/>
+                  <i class="cursor-pointer
+                     bg-black
+                     text-white
+                     px-4
+                     rounded-tr-md
+                     rounded-br-md
+                     flex
+                     items-center
+                     fas fa-chevron-right"></i>
+              </div>
+          
+          </div>
+
+          
+          <IPInfo class="z-20"/>
+      </div>
+
+      <div id="map" class="h-full z-10"></div>
+  </div>
+</template>
+
+<script>
+    import IPInfo from "../components/IPInfo.vue"
+    import leaflet from "leaflet"
+    import { onMounted } from "vue";
+
+export default {
+  name: 'HomeView',
+  components: { IPInfo },
+  setup() {
+      var map;
+
+      onMounted(() => {
+          map = leaflet.map('map').setView([51.505, -0.09], 13);
+          leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              maxZoom: 19,
+              attribution: '© OpenStreetMap'
+          }).addTo(map);
+      })
+
+  },
+}
+</script>
